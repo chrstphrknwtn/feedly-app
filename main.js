e267d53852f9e233ab92569703f96fca4695577a
Christopher Newton <christopher.k.newton@me.com>
2017-02-26 17:01:16 +0100

    Init

---
+++ main.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const electron = require('electron');
+const shell = require('electron').shell;
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const ipc = electron.ipcMain;
+
+// Create the browser window.
+let mainWindow;
+
+function emitUnreadCount() {
+	mainWindow.webContents.executeJavaScript(`
+    require('electron').ipcRenderer.send('unread', document.querySelector('.simpleUnreadCount').innerHTML);
+	`);
+}
+
+function createMainWindow() {
+	mainWindow = new BrowserWindow({
+		width: 1260,
+		minWidth: 1080,
+		height: 1200,
+		titleBarStyle: 'hidden-inset',
+		webPreferences: {allowDisplayingInsecureContent: true},
+		show: false
+	});
+
+	mainWindow.loadURL('https://feedly.com');
+
+	// Add some custom CSS and show mainWindow
+	mainWindow.webContents.on('did-finish-load', () => {
+		mainWindow.webContents.insertCSS(`
+			.dark #feedlyTabsHolder,
+			.dark #feedlyTabs,
+			.dark #addContentPlaceholderFX button {
+				background-color:#181818 !important
+			}
+			#integrationstab_header,
+			#integrationstab_icon,
+			#integrationstab_label,
+			#librarytab,
+			#searchBarFX button.pro {
+				display: none !important
+			}
+			html,body,h1,h2,h3,h4,h5,h6,p,ul,ol,li,
+			h1 a,
+			.title,
+			.fx .entry .summary,
+			.fx .button,.fx button,.fx-button {
+				font-family: BlinkMacSystemFont !important;
+				letter-spacing: 0 !important;
+			}
+			#searchBarFX,
+			#headerBarFX {
+				-webkit-app-region: drag;
+			}`);
+		mainWindow.show();
+	});
+
+	mainWindow.webContents.on('did-navigate-in-page', emitUnreadCount);
+	mainWindow.webContents.on('dom-ready', emitUnreadCount);
+
+	// Open external links in default browser
+	mainWindow.webContents.on('new-window', (e, url) => {
+		if (url.indexOf('feedly.com') < 0) {
+			e.preventDefault();
+			shell.openExternal(url);
+		}
+	});
+
+	// Don't destroy main window when clicking close button in title bar.
+	mainWindow.on('close', e => {
+		e.preventDefault();
+		mainWindow.hide();
+	});
+}
+
+// Create mainWindow
+app.on('ready', () => {
+	createMainWindow();
+});
+
+// Set dock icon to unread count.
+// This is kind of pointless as the 'unread'
+// event is triggered by interaction with the app, and also because feedly
+// does not asyncronously update the unread count (you have to interact with
+// the page).
+// It's only good for starting up the app (which seems to not work 100% of the
+// time) and for getting a 'last known value' when switching to another app.
+ipc.on('unread', (_, value) => {
+	let count = parseInt(value, 10);
+	app.dock.setBadge(count.toString());
+});
+
+// Click on dock icon brings back mainWindow
+app.on('activate', () => {
+	mainWindow.show();
+});
+
+// Kill everything with cmd+q / actually quit
+app.on('before-quit', () => {
+	mainWindow.destroy();
+});
+
+// Quit gracefully
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
---
+++ readme.md
@@ -0,0 +1,15 @@
+![feedly.app](icon/feedly-128.png)
+
+# Feedly app
+
+A basic [electron](http://electron.atom.io) app so [feedly](feedly.com) looks just how I
+like it.
+
+![feedly screenshot](screenshot.png)
+
+## Features
+
+- Decent icon
+- Unread count icon badge
+- External links open in default browser
+- Custom CSS injection
