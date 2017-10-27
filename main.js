696de80c66d3ed3893914129d6f4b9f7bbed4a2e
Christopher Newton <christopher.k.newton@me.com>
2017-10-27 15:31:50 +0200

    Update icon for 10.13 and add copypastaaaa

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 const electron = require('electron');
 const shell = require('electron').shell;
+const globalShortcut = require('electron').globalShortcut;
+const Menu = require('electron').Menu;
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -106,6 +108,32 @@ function createMainWindow() {
 // Create mainWindow
 app.on('ready', () => {
 	createMainWindow();
+
+	// Shortcuts
+	globalShortcut.register('CommandOrControl+W', () => {
+    mainWindow.hide();
+  });
+
+	const template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Feedly", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
 
 // Set dock icon to unread count.
